4400cf9805755f8a9b1dce4639c42ce46967feb4
Shivaal Roy <shivaalroy@gmail.com>
2015-02-21 12:56:43 -0800

    About to add Task.h and Task.m Objective-C files

--- Jeeves/ImageProcessingViewController.swift
+++ Jeeves/ImageProcessingViewController.swift
@@ -27,98 +27,6 @@ class ImageProcessingViewController: UIViewController, UIImagePickerControllerDe
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-//        // HTTP Request
-//        let url = NSURL(string: "http://cloud.ocrsdk.com/processImage")
-//        let request = NSMutableURLRequest(URL: url!)
-//        request.HTTPMethod = "POST"
-//        let applicationID = "Jeeves01"
-//        let applicationPassword = "qk6XJKf91+ZQ6DP2a39C/uI2"
-//        let loginString = NSString(format: "%@:%@", applicationID, applicationPassword)
-//        let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
-//        let base64LoginString = loginData.base64EncodedStringWithOptions(nil)
-//        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-//        
-//        var err: NSError?
-//        request.HTTPBody = UIImageJPEGRepresentation(photo!, 1.0)
-//        
-//        var session = NSURLSession.sharedSession()
-//
-//        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-//            println("Response: \(response)")
-//            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
-//            println("Body: \(strData!) \n")
-//            var err: NSError?
-//            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
-//            
-//            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
-//            if(err != nil) {
-////                println(err!.localizedDescription)
-//                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-//                println("Error could not parse JSON: '\(jsonStr!)'")
-//            }
-//            else {
-//                // The JSONObjectWithData constructor didn't return an error. But, we should still
-//                // check and make sure that json has a value using optional binding.
-//                if let parseJSON = json {
-//                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
-//                    var success = parseJSON["success"] as? Int
-//                    println("Succes: \(success)")
-//                }
-//                else {
-//                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-//                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-//                    println("Error could not parse JSON: \(jsonStr)")
-//                }
-//            }
-//        })
-//        
-//        task.resume()
-//        
-//        
-//        let url2 = NSURL(string: "http://cloud.ocrsdk.com/getApplicationInfo")
-//        let request2 = NSMutableURLRequest(URL: url2!)
-//        request2.HTTPMethod = "GET"
-//        request2.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-//        
-//        let err2: NSError?
-//        request2.HTTPBody = UIImageJPEGRepresentation(photo!, 1.0)
-//        
-//        var session = NSURLSession.sharedSession()
-//        
-//        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-//            println("Response: \(response)")
-//            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
-//            println("Body: \(strData!) \n")
-//            var err: NSError?
-//            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
-//            
-//            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
-//            if(err != nil) {
-//                //                println(err!.localizedDescription)
-//                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-//                println("Error could not parse JSON: '\(jsonStr!)'")
-//            }
-//            else {
-//                // The JSONObjectWithData constructor didn't return an error. But, we should still
-//                // check and make sure that json has a value using optional binding.
-//                if let parseJSON = json {
-//                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
-//                    var success = parseJSON["success"] as? Int
-//                    println("Succes: \(success)")
-//                }
-//                else {
-//                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-//                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-//                    println("Error could not parse JSON: \(jsonStr)")
-//                }
-//            }
-//        })
-        
-//        let urlConnection = NSURLConnection(request: request, delegate: self)
-//        if urlConnection != nil {
-//            println("UrlConnection is not nil")
-//        }
     }
 
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
@@ -126,7 +34,56 @@ class ImageProcessingViewController: UIViewController, UIImagePickerControllerDe
         self.photo = info[UIImagePickerControllerOriginalImage] as UIImage?
         if self.photo != nil {
             println("Photo is not nil")
-            // DO PHOTO TO TEXT STUFF HERE
+            
+            // HTTP Request
+            let url = NSURL(string: "http://cloud.ocrsdk.com/processImage?language=English&exportFormat=txt")
+            //        let url = NSURL(string: "http://cloud.ocrsdk.com/getApplicationInfo")
+            let request = NSMutableURLRequest(URL: url!)
+            request.HTTPMethod = "POST"
+            let applicationID = "Jeeves01"
+            let applicationPassword = "qk6XJKf91+ZQ6DP2a39C/uI2"
+            let loginString = NSString(format: "%@:%@", applicationID, applicationPassword)
+            let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
+            let base64LoginString = loginData.base64EncodedStringWithOptions(nil)
+            request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+            request.setValue("applicaton/octet-stream", forHTTPHeaderField: "Content-Type")
+            
+            var err: NSError?
+            request.HTTPBody = UIImageJPEGRepresentation(photo!, 1.0)
+            
+            
+            var session = NSURLSession.sharedSession()
+            
+            var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+                println("Response: \(response)")
+                var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Body: \(strData!) \n")
+                var err: NSError?
+                var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+                
+                // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+                if(err != nil) {
+                    //                println(err!.localizedDescription)
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("Error could not parse JSON: '\(jsonStr!)'")
+                }
+                else {
+                    // The JSONObjectWithData constructor didn't return an error. But, we should still
+                    // check and make sure that json has a value using optional binding.
+                    if let parseJSON = json {
+                        // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                        var success = parseJSON["success"] as? Int
+                        println("Succes: \(success)")
+                    }
+                    else {
+                        // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                        let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                        println("Error could not parse JSON: \(jsonStr)")
+                    }
+                }
+            })
+            
+            task.resume()
         }
     }
     
